e2c4763aa4cc90bd422c24cc79e5409256137d25
Miroslav Popov <info@forexsb.com>
2013-03-17 22:00:11 +0200

    FSB Version updated to v2.77.3 Beta

--- Source/ForexStrategyBuilder/Properties/AssemblyInfo.cs
+++ Source/ForexStrategyBuilder/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.77.2.0")]
-[assembly: AssemblyFileVersion("2.77.2.0")]
+[assembly: AssemblyVersion("2.77.3.0")]
+[assembly: AssemblyFileVersion("2.77.3.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
